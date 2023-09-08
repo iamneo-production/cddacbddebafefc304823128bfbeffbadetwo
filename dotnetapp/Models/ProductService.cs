d2c6b82c5db8192418c09581bfd02b3e1c1aaab9
Iamneo <techadmin@iamneo.ai>
2023-09-08 11:54:52 +0000

    Backup Commit

---
+++ dotnetapp/Models/ProductService.cs
@@ -0,0 +1,37 @@
+using dotnetapp.Models;
+using System.Linq;
+
+namespace dotnetapp.Models
+{
+    public interface IProductService
+    {
+        public IQueryable<Product> GetProductList();
+        public bool AddProduct(Product product);
+        public bool DeleteProduct(int Id);
+    }
+    //Fill ur code
+    public class ProductService : IProductService
+    {
+       private readonly ProductDBContext _dbContext;
+
+        public ProductService(ProductDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IQueryable<Product> GetProductList()
+        {
+           return _dbContext.Product();
+        }
+        public bool AddProduct(Product product)
+        {
+            product.add(Product);
+            
+            return false;
+        }
+
+        public bool DeleteProduct(int Id)
+        {
+            return false;
+        }
+    }
+}
